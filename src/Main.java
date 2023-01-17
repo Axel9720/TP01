e489d3e8d8445d0532653abe41e4910a8b317e2f
Axel <axel.augustine972@gmail.com>
2023-01-17 15:35:49 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    double prix1=11.5;
+    double prix2=8.7;
+    int prix3 = 4;
+
+    double Total = prix1+prix2+prix3;
+    System.out.println(Total);
+
+}
